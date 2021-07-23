c986f08ae00093178012a7f2a2258afc94ca5fa6
liwei <2552845664@qq.com>
2021-07-23 17:38:48 +0800

    项目初始化引入Vant组件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入Vant ui组件
+import Vant from 'vant'
+// 导入Vant css
+import 'vant/lib/index.css'
+
+import './styles/index.less'
+
+// 挂在启用Vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
